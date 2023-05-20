e6032a32f093ea1e5289a3e1847f87867afaeb20
PseudoElement <bubamainer@mail.ru>
2023-05-20 21:03:14 +0300

    add behaviour subject counter

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { BehaviorSubject, Observable, Subject, Subscription, first, map } from "rxjs";
 import { ButtonTypes } from "src/app/shared/enums";
 
 @Component({
@@ -11,6 +12,17 @@ export class AboutComponent {
           return ButtonTypes;
      }
      public isOpened = false;
+
+     value$: BehaviorSubject<number> = new BehaviorSubject(2);
+     count?: number;
+     subscription: Subscription = this.value$.pipe(map((val) => val * 2)).subscribe((val) => (this.count = val));
+
+     onClick() {
+          this.value$.next(this.value$.value + 1);
+     }
+     ngOnDestroy() {
+          this.subscription.unsubscribe();
+     }
 
      openModal() {
           this.isOpened = true;
